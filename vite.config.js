deebf3682b0e5c94d517d99ca5394005633d2bfe
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-11-14 15:27:56 +0100

    Deploy website

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./business-cards-react",
+  base: "/business-cards-react",
   plugins: [react()],
 });
