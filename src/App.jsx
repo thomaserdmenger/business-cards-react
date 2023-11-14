a7878296f9e259db40540301978e472da888a667
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-11-14 10:01:53 +0100

    Fetch data into card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import * as React from "react";
+
 import Card from "./components/Card";
 import "./styles.css";
 
 export default function App() {
+  const [id, setId] = React.useState(1);
+  const [user, setUser] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState("");
+
+  React.useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users/${id}`,
+        { signal }
+      );
+
+      const data = await response.json();
+      setUser(data);
+      setLoading(false);
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [id]);
+
   return (
     <main>
-      <Card />
+      <Card user={user} />
     </main>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 
-export default function Card() {
+export default function Card({ user }) {
+  const { id, name, username, phone, email, website } = user;
+
   return (
     <React.Fragment>
-      <div className="card-container">
+      <div key={id} className="card-container">
         <div className="user-data-top">
-          <p className="user-name">Thomas Erdmenger</p>
-          <p className="user-handle">@thomaserdmenger</p>
+          <p className="user-name">{name}</p>
+          <p className="user-handle">{username}</p>
         </div>
-        <div className="user-data-middle">
-          <p className="user-street">Zinksgartenstraße 13</p>
-          <p className="user-city">06108 Halle</p>
-        </div>
+        <div className="user-data-middle"></div>
         <div>
-          <p className="user-phone">Phone: 0151 ...</p>
-          <p className="user-website">Website: www.google.de</p>
+          <p className="user-phone">Phone: {phone}</p>
+          <p className="user-phone">Email: {email}</p>
+          <p className="user-website">Website: {website}</p>
         </div>
       </div>
       <div className="buttons">
